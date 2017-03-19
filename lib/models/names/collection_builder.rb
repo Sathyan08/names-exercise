95c786d66587d923caef25575a1c1ba0d37baee5
Sathyan Mathai <smathai@salsify.com>
2017-03-18 23:00:25 -0400

    Added comments

--- lib/models/names/collection_builder.rb
+++ lib/models/names/collection_builder.rb
@@ -2,20 +2,26 @@ module Names
   class CollectionBuilder
     LAST_NAME_COLUMN = 0
     FIRST_NAME_COLUMN = 1
-    CHARACTERS_REGEX = /[a-zA-Z]/
-    NON_CHARACTERS_REGEX = /[^a-zA-Z]/
+    LETTERS_REGEX = /[a-zA-Z]/
+    NON_LETTERS_REGEX = /[^a-zA-Z]/
 
     def self.build(file_path, unique_names_list_length = 25)
       name_collection = Names::Collection.new(unique_names_list_length)
 
       File.readlines(file_path).each do |line|
         # Checks to see if the line starts with a character that might be in a valid name.
-        next unless CHARACTERS_REGEX.match(line[0]).present?
+        next unless LETTERS_REGEX.match(line[0]).present?
 
-        line.gsub!(NON_CHARACTERS_REGEX, ' ')
+        # Take the first two words
         row = line.split(' ')
         first_name = row[FIRST_NAME_COLUMN].strip
         last_name = row[LAST_NAME_COLUMN].strip
+
+        # Cuts off the commas off of the last name
+        last_name = last_name[0..-2] if last_name.end_with?(',')
+
+        # skips the line if the first two words contain any non-letter characters
+        next if NON_LETTERS_REGEX.match(first_name) || NON_LETTERS_REGEX.match(last_name)
 
         if first_name.present? && last_name.present?
           name = Names::Name.new(first_name, last_name)
